fc96b94d13b2bbbb2f2c0d92c6b09dca9e19cee2
mira6ts-star <mira6ts@bu.edu>
2026-04-08 12:44:07 -0400

    adding comments

--- project_3_client_server_analytics/server/src/solution.rs
+++ project_3_client_server_analytics/server/src/solution.rs
@@ -5,11 +5,16 @@ pub fn hello() -> String {
     return String::from("hello");
 }
 
+// clones the entire dataset and sents it back
+// slow bc potentially sending thousands of rows over the network for no reason
 pub fn slow_rpc(input_dataset: &Dataset) -> Dataset {
     println!("slow_rpc called");
     input_dataset.clone()
 }
 
+// server recieves query
+// runs compute_query_on_dataset and sends back small result
+// client gets a tiny dataset instead of a huge one
 pub fn fast_rpc(input_dataset: &Dataset, query: Query) -> Dataset {
     println!("fast_rpc called");
     analytics_lib::solution::compute_query_on_dataset(input_dataset, &query)
